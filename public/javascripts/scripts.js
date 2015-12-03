21211424ddc0646671221e3c08fab3d6cd07ecb1
Pedro Castilho <pedro.castilho@rwth-aachen.de>
2015-12-03 04:03:46 -0300

    seletores da landing page

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,23 +1,30 @@
 $('.selectpicker').selectpicker({
-     style: 'btn-default'
- });
-
+    style: 'btn-default'
+});
 
 $('#cargo').change(function(){
-  if ($(this).val() === "Presidente"){
-    // Manipular o DOM para remover os estados, permitindo apenas Brasil
-    console.log("Presidente")
-  $('#uf').filter(function(){return $(this).text()!=="Brasil"}).prop('disabled',true);
-  $('#uf').filter(function(){return $(this).text()==="Brasil"}).prop('disabled',false);
-  $('#uf').selectpicker('refresh');
+    if ($(this).val() === "Presidente"){
+	// Manipular o DOM para remover os estados, permitindo apenas Brasil
+	console.log("Presidente")
+	$('#uf').val("Brasil");
+	$('#uf').children().filter(function(){return $(this).text()==="Brasil"}).prop('disabled',false);
+	$('#uf').children().filter(function(){return $(this).text()!=="Brasil"}).prop('disabled',true);
+	$('#uf').selectpicker('refresh'); 
 
-} else if($(this).val() === "Governador"){
-  console.log("Governador")
-  // Manipular o DOM para restaurar estados e remover Brasil
-  $('#uf').filter(function(){return $(this).text()!=="Brasil"}).prop('disabled',false);
-  $('#uf').filter(function(){return $(this).text()==="Brasil"}).prop('disabled',true);
-  $('#uf').selectpicker('refresh');
+    } else if($(this).val() === "Governador"){
+	console.log("Governador")
+	// Manipular o DOM para restaurar estados e remover Brasil
+	$('#uf').val("Pernambuco");
+	$('#uf').children().filter(function(){return $(this).text()==="Brasil"}).prop('disabled',true);
+        $('#uf').children().filter(function(){return $(this).text()!=="Brasil"}).prop('disabled',false);
+	$('#uf').selectpicker('refresh');
+
+    }
+});
+
+//sempre começamos em governador, portanto fazer a devida desativação
+$('#uf').children().filter(function(){return $(this).text()==="Brasil"}).prop('disabled',true);
+$('#uf').children().filter(function(){return $(this).text()!=="Brasil"}).prop('disabled',false);
+$('#uf').selectpicker('refresh');
 
 
-  }
-})
